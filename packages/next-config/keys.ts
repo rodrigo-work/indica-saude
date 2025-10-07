e4f23e21aa84cc90ba26ddc61ebab179a70f3e56
Rodrigo Ribeiro <me@rodrigo3d.com>
2025-10-07 02:47:22 -0300

    chore(next-config): general updates to the packages/next-config

---
+++ packages/next-config/keys.ts
@@ -0,0 +1,30 @@
+import { vercel } from '@t3-oss/env-core/presets-zod'
+import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod'
+
+export const keys = () =>
+  createEnv({
+    extends: [vercel()],
+    server: {
+      JWT_SECRET: z.string(),
+
+      ANALYZE: z.string().optional(),
+      // Added by Vercel
+      NEXT_RUNTIME: z.enum(['nodejs', 'edge']).optional()
+    },
+    client: {
+      NEXT_PUBLIC_WEB_URL: z.string().url(),
+      NEXT_PUBLIC_APP_URL: z.string().url(),
+      NEXT_PUBLIC_API_URL: z.string().url()
+    },
+    runtimeEnv: {
+      JWT_SECRET: process.env.JWT_SECRET,
+
+      ANALYZE: process.env.ANALYZE,
+      NEXT_RUNTIME: process.env.NEXT_RUNTIME,
+
+      NEXT_PUBLIC_WEB_URL: process.env.NEXT_PUBLIC_WEB_URL,
+      NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
+    }
+  })

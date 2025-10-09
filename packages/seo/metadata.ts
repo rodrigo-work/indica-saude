34cdf057a019ce4a6ea94bee90eb5c08664db8a6
Rodrigo Ribeiro <me@rodrigo3d.com>
2025-10-08 23:35:29 -0300

    chore(seo): general updates to the packages/seo

---
+++ packages/seo/metadata.ts
@@ -0,0 +1,70 @@
+import merge from 'lodash.merge'
+import type { Metadata } from 'next'
+
+type MetadataGenerator = Omit<Metadata, 'description' | 'title'> & {
+  title: string
+  description: string
+  image?: string
+}
+
+const applicationName = 'Indica Saúde'
+const author: Metadata['authors'] = {
+  name: 'Indica Saúde',
+  url: 'https://indica-saude.vercel.app/'
+}
+const publisher = 'rodrigo.work'
+const twitterHandle = '@rodrigo_work'
+const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http'
+const productionUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL
+
+export const createMetadata = ({
+  title,
+  description,
+  image,
+  ...properties
+}: MetadataGenerator): Metadata => {
+  const parsedTitle = `${title} | ${applicationName}`
+  const defaultMetadata: Metadata = {
+    title: parsedTitle,
+    description,
+    applicationName,
+    metadataBase: productionUrl ? new URL(`${protocol}://${productionUrl}`) : undefined,
+    authors: [author],
+    creator: author.name,
+    formatDetection: {
+      telephone: false
+    },
+    appleWebApp: {
+      capable: true,
+      statusBarStyle: 'default',
+      title: parsedTitle
+    },
+    openGraph: {
+      title: parsedTitle,
+      description,
+      type: 'website',
+      siteName: applicationName,
+      locale: 'en_US'
+    },
+    publisher,
+    twitter: {
+      card: 'summary_large_image',
+      creator: twitterHandle
+    }
+  }
+
+  const metadata: Metadata = merge(defaultMetadata, properties)
+
+  if (image && metadata.openGraph) {
+    metadata.openGraph.images = [
+      {
+        url: image,
+        width: 1200,
+        height: 630,
+        alt: title
+      }
+    ]
+  }
+
+  return metadata
+}
